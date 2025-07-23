05f45fd7284283de66baa14172bba1cde72ce27d
TheSkylerDev <kesch.inno14@gmail.com>
2025-07-23 16:31:08 +0200

    Update barrelsize.lua

--- lua/barrelsize.lua
+++ lua/barrelsize.lua
@@ -4,7 +4,7 @@ height = 0.0
 volume = 0.0
 area   = 0.0
 
-function main(radius, height)
+function calc(radius, height)
     --execute calculations using 'math.pi'
     volume = math.pi * radius^2 * height
     area   = 2 * math.pi * radius * (radius * height)
@@ -15,11 +15,14 @@ function main(radius, height)
     print(area)
 end
 
---taking user input globally
-print("Enter radius in centimeters: ")
-radius = io.read()
-radius = tonumber(radius)
-print("Enter height in centimeters: ")
-height = io.read()
-height = tonumber(height)
-main(radius, height) --call main function
+function main()
+    --taking user input globally
+    print("Enter radius in centimeters: ")
+    radius = io.read()
+    radius = tonumber(radius)
+    print("Enter height in centimeters: ")
+    height = io.read()
+    height = tonumber(height)
+    calc(radius, height) --call main function
+end
+main()
