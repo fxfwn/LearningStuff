280666c73f10108faf20efddfd43e09023ba52d1
TheSkylerDev <kesch.inno14@gmail.com>
2025-07-23 10:19:17 +0200

    update firstlua.lua

--- lua/firstlua.lua
+++ lua/firstlua.lua
@@ -5,4 +5,22 @@ function greet()
     print("Hello, World")
 end
 
+function coolStuff()
+    local food = "Pomelo"
+    local foods = {fave01 = "Fries", fave02 = "Shnitzel", fave03 = "Sushi"}
+    print(#food) --prints the length of "Pomelo"
+    print(foods.fave01)
+    print(foods.fave02)
+    print(foods.fave03)
+end
+
+function numbers()
+    math.randomseed(os.time())
+    local random_num = math.random()
+    print(random_num)
+    print("Random Number!")
+end
+
 greet()
+coolStuff()
+numbers()
